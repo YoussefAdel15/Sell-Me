e86b63fa4b5d314157d7b045299b67306cacae84
Tefa600 <67193168+Tefa600@users.noreply.github.com>
2021-12-19 23:01:03 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 
+const User = require("../../models/User");
+
 //@route        POST api/users
 //@desc         Register User
 //@acess        public
@@ -14,12 +16,31 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exist
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exist" }] });
+      }
+      //Get users gravatar
+
+      //encrypt password
+
+      //return jasonwebtoken
+      res.send("User route");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
   }
 );
 

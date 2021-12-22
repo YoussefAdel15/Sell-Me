76280e00c814e6c3433757c9907c0c576de873b3
Tefa600 <67193168+Tefa600@users.noreply.github.com>
2021-12-22 11:43:08 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
@@ -29,14 +31,34 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exist" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exist" }] });
       }
       //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
 
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
       //encrypt password
 
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
       //return jasonwebtoken
-      res.send("User route");
+
+      res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
